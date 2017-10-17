ff7e408de9d21d96a59ae282fe5e2d536dcd4590
kuba465 <jakub.k.paluszkiewicz@gmail.com>
2017-10-17 18:39:59 +0200

    create Size at weekend

--- class/Address.php
+++ class/Address.php
@@ -1,7 +1,135 @@
 <?php
 
-class Address implements Action
-{
+class Address implements Action {
 
-    
+    /**
+     *
+     * @var DBmysql $db
+     */
+    public static $db;
+    private $id = -1;
+    private $city = '';
+    private $code = '';
+    private $street = '';
+    private $number = '';
+
+    function getId() {
+        return $this->id;
+    }
+
+    function getCity() {
+        return $this->city;
+    }
+
+    function getCode() {
+        return $this->code;
+    }
+
+    function getStreet() {
+        return $this->street;
+    }
+
+    function getNumber() {
+        return $this->number;
+    }
+
+    function setCity($city) {
+        if (!is_string($city) || strlen($city) < 0) {
+            echo "Złe miasto.";
+            return false;
+        }
+        $this->city = $city;
+    }
+
+    function setCode($code) {
+        if (!is_string($code) || strlen($code) < 0) {
+            echo "Zły kod pocztowy.";
+            return false;
+        }
+        $this->code = $code;
+    }
+
+    function setStreet($street) {
+        if (!is_string($street) || strlen($street) < 0) {
+            echo "Zła ulica.";
+            return false;
+        }
+        $this->street = $street;
+    }
+
+    function setNumber($number) {
+        if (!is_string($number) || strlen($number) < 0) {
+            echo "Zły numer.";
+            return false;
+        }
+        $this->number = $number;
+    }
+
+    public function delete() {
+        $address = Address::load($this->getId());
+        $sql = "DELETE FROM Addresses WHERE id=" . $address->getId();
+        self::$db->query($sql);
+        self::$db->execute();
+    }
+
+    public function save() {
+        if ($this->id > 0) {
+            $this->update();
+        } else {
+            $sql = "INSERT INTO Addresses SET city=:city, code=:code, street=:street, number=:number";
+            self::$db->query($sql);
+            self::$db->bind('city', $this->getCity());
+            self::$db->bind('code', $this->getCode());
+            self::$db->bind('street', $this->getStreet());
+            self::$db->bind('number', $this->getNumber());
+            self::$db->execute();
+        }
+    }
+
+    public function update() {
+        $sql = "UPDATE Addresses SET city=:city, code=:code, street=:street, number=:number WHERE id=:id";
+        self::$db->query($sql);
+        self::$db->bind('id', $this->getId());
+        self::$db->bind('city', $this->getCity());
+        self::$db->bind('code', $this->getCode());
+        self::$db->bind('street', $this->getStreet());
+        self::$db->bind('number', $this->getNumber());
+        self::$db->execute();
+    }
+
+    public static function load($id = null) {
+        $sql = "SELECT * FROM Addresses WHERE id=:id";
+        self::$db->query($sql);
+        self::$db->bind('id', $id);
+        $singleAddress = self::$db->single();
+
+        $address = new Address();
+        $address->setCity($singleAddress['city']);
+        $address->setCode($singleAddress['code']);
+        $address->setStreet($singleAddress['street']);
+        $address->setNumber($singleAddress['number']);
+    }
+
+    public static function loadAll() {
+        $addressesList = [];
+        $sql = "SELECT * FROM Addresses";
+        self::$db->query($sql);
+        $allAddressesFromDb = self::$db->resultSet();
+
+        foreach ($allAddressesFromDb as $address) {
+            $addressesList[] = [
+                'id' => $address['id'],
+                'city' => $address['city'],
+                'code' => $address['code'],
+                'street' => $address['street'],
+                'flat' => $address['number'],
+            ];
+        }
+        return $addressesList;
+    }
+
+    public static function setDb(\Database $db) {
+        self::$db = $db;
+    }
+
 }
--- config.php
+++ config.php
@@ -6,13 +6,13 @@ require(__DIR__ . '/class/interface/Action.php');
 
 require(__DIR__ . '/class/database/DBmysql.php');
 
-require(__DIR__ . '/class/Parcel.php');
+//require(__DIR__ . '/class/Parcel.php');
 require(__DIR__ . '/class/Size.php');
-require(__DIR__ . '/class/Address.php');
-require(__DIR__ . '/class/User.php');
+//require(__DIR__ . '/class/Address.php');
+//require(__DIR__ . '/class/User.php');
 
 //Set DB connection
-define('DB_SERVER_NAME', '');
-define('DB_USERNAME', '');
-define('DB_PASSWORD', '');
-define('DB_BASE_NAME', '');
+define('DB_SERVER_NAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'coderslab');
+define('DB_BASE_NAME', 'warsztat4');
