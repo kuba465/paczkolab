6a51d4cf82e0f7ecac2be7879d40f4135f065422
kuba465 <jakub.k.paluszkiewicz@gmail.com>
2017-10-25 17:57:12 +0200

    config?

--- config.php
+++ config.php
@@ -15,5 +15,5 @@ require (__DIR__ . '/class/database/DBmysql.php');
 define('DB_SERVER_NAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'coderslab');
-define('DB_BASE_NAME', 'paczkolab');
+define('DB_BASE_NAME', 'warsztat4');
 
